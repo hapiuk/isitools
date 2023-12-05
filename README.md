8fbe9b7b136e32c81d6f857d73b4f7595bbe0844
Aaron Gomm <aaron.gomm@outlook.com>
2023-12-05 10:26:28 +0000

    Project Upload

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# isitools
-ISI Dashboard, Tools and Tracking (Internal Use Only)
+# ISI Dashboard 
+
+This project is a Flask-based web application that allows users to upload PDF files, processes these files, stores and retrieves quiz results, and downloads processed files.
+
+## Installation
+
+This project requires Python 3.6 or later. Clone the repository and install the required packages:
+
+```
+git clone https://github.com/aarongommisi/isidashboard.git
+cd isidashboard
+pip install -r requirements.txt
+
+```
+
+# Usage
+
+## app.py
+
+This script runs the main application. It serves the HTML pages, handles the file upload, processes these files with a separate script, and allows users to download the results.
+
+```
+python app.py
+```
+
+## pdfextract.py
+
+This script is used to extract data from uploaded PDFs. It extracts specific information from the text of the PDFs, writes this extracted information to a CSV file, merges processed and faulty PDFs into separate files, and moves the processed PDFs to a separate folder.
+
+```
+python pdfextract.py --input input_folder --output output_folder --processed processed_folder --business_entity business_entity
+```
+
+*Note: Replace input_folder, output_folder, processed_folder, and business_entity with your actual parameters.*
+
+
+# Web Pages
+
+
+## index.html
+
+This is the main dashboard of the application. It provides links to other resources or parts of the application such as the Company Website, Service Sight, Lucid App, PDF Data Extraction Tool, Remedial Actions CSV Generator, Incident Report CSV Generator, Suggestion Request, Support Request, SAFED Mock Test, and Leaderboard.
+
+## upload.html
+
+This page allows users to upload PDF files to be processed. Users are also required to provide a business entity name. The page includes a JavaScript script that handles form submission, sends the uploaded files and business entity name to the server, and redirects the user to download the processed files.
+
+## mockexam.html
+
+This page contains the SAFED Mock Test for LOLER (WiP). It includes functionality for displaying questions, selecting answers, calculating scores, and generating certificates.
+
+## leaderboard.html
+
+This page displays the leaderboard for the mock exam. It shows the rank, name, score, total questions, and percentage for each participant.
+
+
+
+
+
---
+++ static/questions.js
@@ -0,0 +1,181 @@
+ const questions = [{
+    question: "What year were the current requirements for LLMC become mandatory for variable reach lift tucks Telehandlers?",
+    options: ["2008", "2010", "2012", "2013"],
+    answer: "2010"
+  },
+  {
+    question: "After how many years of service does BS7121 state that lorry loader cranes should be thoroughly examined 6 monthly?",
+    options: ["5", "6", "8", "10"],
+    answer: "8"
+  },
+  {
+    question: "What is the minimum diameter for the sheave of a traction lift if the suspension ropes are 10mm nominal diameter?",
+    options: ["300 mm", "350 mm", "400 mm", "500 mm"],
+    answer: "400 mm"
+  },
+  {
+    question: "What is the minimum clearance between the counterweight and lift car?",
+    options: ["35 mm", "40 mm", "45 mm", "50 mm"],
+    answer: "50 mm"
+  },
+  {
+    question: "How much mechanical engagement should a lift landing door interlock have before electrical contacts are made?",
+    options: ["5 mm", "6 mm", "7 mm", "8 mm"],
+    answer: "7 mm"
+  },
+  {
+    question: "Which one of these is not a type of chain block?",
+    options: ["Weston block", "Triple geared chain block", "Pul-lift", "Snatch block"],
+    answer: "Snatch block"
+  },
+  {
+    question: "Which of these is not a type of lift rope sheave groove?",
+    options: ["V", "Undercut U", "Undercut J", "U"],
+    answer: "Undercut J"
+  },
+  {
+    question: "What of the following is one of the minimum refuge spaces in a lift pit?",
+    options: ["0.5 x 0.7 x 1.0 m", "0.4 x 0.7, x 1.1 m", "0.6 x 0.8 x 1.1 m", "0.45 x 0.6 x 1.0 m"],
+    answer: "0.5 x 0.7 x 1.0 m"
+  },
+  {
+    question: "What is the minimum tripping speed of an overspeed governor in terms of contract speed?",
+    options: ["110%", "115%", "120%", "125%"],
+    answer: "115%"
+  },
+  {
+    question: "The definition of a service lift by maximum volume is?",
+    options: ["1.0 m sq floor area max ht 1.2 m", "1.2 m sq floor area max ht 1.0 m", "0.8 m sq floor area max ht 1.2 m", "1.0 m sq floor area max ht 1.0 m"],
+    answer: "1.0 m sq floor area max ht 1.2 m"
+  },
+  {
+    question: "What is the minimum factor of safety for lift suspension ropes (when over 3 are fitted)?",
+    options: ["16", "12", "10", "5"],
+    answer: "12"
+  },
+  {
+    question: "Which of the following would you not expect to see on a new excavator?",
+    options: ["A fully automatic quick hitch", "A lifting pint with SWL marked on the quick hitch", "An automatic hitch warning / information decal in the excavator cab", "A semi-automatic quick hitch"],
+    answer: "A semi-automatic quick hitch"
+  },
+  {
+    question: "What is the maximum allowable wear on a chain link of a chain sling?",
+    options: ["8% wear", "8% wear average across two axis at 90 degrees to each other", "10% wear", "10% wear average across two axis at 90 degrees to each other"],
+    answer: "10% wear average across two axis at 90 degrees to each other"
+  },
+  {
+    question: "Which one of these is not an electrical safety device on a passenger lift?",
+    options: ["Pit stop switch", "Car overload detection", "Car Alarm", "Stored position pit ladder switch"],
+    answer: "Car Alarm"
+  },
+  {
+    question: "What is the maximum deflection as a proportion of span under rated capacity load test on an overhead crane bridge?",
+    options: ["1/250", "1/400", "1/500", "1/750"],
+    answer: "1/750"
+  },
+  {
+    question: "Rope rejection criteria for diameter loss due to wear of a lift suspension rope on a traction lift?",
+    options: ["4%", "6%", "10%", "15%"],
+    answer: "6%"
+  },
+  {
+    question: "If a webbing or round sling color is Red, how many tonnes does that represent?",
+    options: ["4", "5", "6", "7"],
+    answer: "5"
+  },
+  {
+    question: "Which of the following wire rope diameters is not suitable for a Wire Rope hoist made by Tractel/Tirfor?",
+    options: ["16.3 mm", "11.5 mm", "6.3 mm", "10.0 mm"],
+    answer: "10.0 mm"
+  },
+  {
+    question: "If an Otis Gen 2 MRL has no Pulse monitoring fitted, when should you ask for replacement/testing in terms of years fitted?",
+    options: ["8 years", "10 years", "12 years", "15 years"],
+    answer: "10 years"
+  },
+  {
+    question: "Which of the following statements regarding factors of safety (FOS) is incorrect?",
+    options: ["Chain sling FOS is 4", "Wire rope sling FOS is 5", "Load Fork FOS is 6", "Round Sling FOS is 7"],
+    answer: "Load Fork FOS is 6"
+  },
+  {
+    question: "Webbing and Round sling label colour identifies the load bearing material, which of the following is incorrect?",
+    options: ["Blue: Polyester", "Green: Nylon/Polyamide", "Brown: Polypropylene", "Violet: Phenolic"],
+    answer: "Violet: Phenolic"
+  },
+  {
+    question: "You find an immediate defect on a crane on hire to a contractor working on a large construction site. LOLER requires us to issue a report to?",
+    options: ["The user", "The customer and the enforcing authority", "The user, the owner, the responsible person representing the site and the HSE", "The user, the owner, the enforcing authority"],
+    answer: "The user, the owner, the responsible person representing the site and the HSE"
+  },
+  {
+    question: "When are 4 broken wires grounds for issuing a timed defect report on lift suspension ropes?",
+    options: ["When small ropes are fitted to a Kone monospace", "When near to a termination", "When in two adjacent strands", "When adjacent on one strand"],
+    answer: "When adjacent on one strand"
+  },
+  {
+    question: "Which of the following may not be an immediate defect on a two post vehicle lift?",
+    options: ["Arm lock inoperative", "Arm lock allows excessive movement of extended arms greater than diameter of pick-up pad", "Two post lift arm lock cam teeth worn and chipped", "Two post vehicle lift manufactured in 1975 without arm locks fitted and none retrofitted"],
+    answer: "Two post lift arm lock cam teeth worn and chipped"
+  },
+  {
+    question: "Which one of the following statements is incorrect? A manual pallet truck with a lift height under 300mm must be:",
+    options: ["Thoroughly Examined under LOLER and reported on a LOLER report form", "Where inspected, it falls under PUWER and should be reported on a suitable PUWER report form in accordance with SAFed guidance note MLCC05", "Considered as part of the employer's risk assessment and inspected where found necessary", "Maintained in a safe condition"],
+    answer: "Thoroughly Examined under LOLER and reported on a LOLER report form"
+  },
+  {
+    question: "A 2500 kg lift truck fitted with load forks which have had heel pads welded to the underside of worn heels. The heel measurement including the pad is greater than the shank thickness. You should:",
+    options: ["Recommend replacement of the load forks", "Issue a report noting welded repairs should be KUOBS", "Advise a cup of tea and a Kit Kat would get a clear examination certificate", "Ask for a load fork/lift truck manufacturer's authorization of the repair, evidence of a satisfactory 2.5 times capacity load test certificate post-repair, and satisfy yourself the repair was carried out by the manufacturer or someone of equal competence before accepting the repair"],
+    answer: "Ask for a load fork/lift truck manufacturer's authorization of the repair, evidence of a satisfactory 2.5 times capacity load test certificate post-repair, and satisfy yourself the repair was carried out by the manufacturer or someone of equal competence before accepting the repair"
+  },
+  {
+    question: "Which of the following does not require a six-monthly examination under LOLER?",
+    options: ["An excavator/material handler with elevating Cab", "A demountable Lift truck e.g. Moffett type", "A scissor vehicle lift with car body straightening jig fitted", "A passenger lift in an apartment building"],
+    answer: "A passenger lift in an apartment building"
+  },
+  {
+    question: "What is the maximum frequency of inspection recommended in HSE INDG367 Inspecting Fall arrest equipment made from webbing or rope made to BS EN 365: 2004 Personal Protective Equipment (PPE) against falls from a height, Guidance for user, maintenance, periodic examination repair, marking and packaging, The document gives guidance general requirement for periodic inspection. This is:",
+    options: ["Inspect at least annually as PPE", "Inspect at least 6 monthly as PPE", "Examine at least 6 monthly under LOLER", "Inspect at periods laid down in PPE Regulations"],
+    answer: "Inspect at least annually as PPE"
+  },
+  {
+    question: "You are examining crane tracks. How far does the examination extend?",
+    options: ["Track rail, end stops, and alignment", "Track rail, end stops, alignment, and hold-down fixings or welds", "Track rail, end stops, alignment, hold-down fixings or welds, and supporting beams/fixings", "Track rail, end stops, alignment, hold-down fixings or welds, supporting beams/fixings, and the building to the extent that the building structure supports the lifting equipment"],
+    answer: "Track rail, end stops, alignment, hold-down fixings or welds, supporting beams/fixings, and the building to the extent that the building structure supports the lifting equipment"
+  },
+  {
+    question: "Which feature of a car top control on a lift is not required to comply with the latest passenger lift manufacturing standard?",
+    options: ["Stop switch", "Hold to run switch", "Car door operation switch", "Inspection/normal changeover switch"],
+    answer: "Car door operation switch"
+  },
+  {
+    question: "What modes of operation are not restricted by a telehandler built to current standards when extended and in an overload state?",
+    options: ["Boom out", "Raise outriggers", "Boom up", "Boom down"],
+    answer: "Boom up"
+  },
+  {
+    question: "Which is not a crawler crane part?",
+    options: ["Rooster", "Bridle", "Chord", "Trello"],
+    answer: "Trello"
+  },
+  {
+    question: "Which is not a term associated with excavators?",
+    options: ["Rubber duck", "Grouser", "Stick", "Gantt"],
+    answer: "Gantt"
+  },
+  {
+    question: "Which legal requirement is a duty under LOLER?",
+    options: ["Stability", "Guarding", "Maintenance", "Isolation of energy sources"],
+    answer: "Stability"
+  },
+  {
+    question: "A modern two-post hydraulic vehicle lift installed in the UK need not have the following feature?",
+    options: ["Top limit switch", "CE or UKCA Mark", "Operating instructions in the English language", "Hold to run controls"],
+    answer: "Top limit switch"
+  },
+  {
+    question: "In which case would you not expect to see an initial examination under LOLER giving details of a load test?",
+    options: ["Vehicle lift", "Pillar crane", "Conveyor", "Lorry loader Crane"],
+    answer: "Conveyor"
+  }
+];
